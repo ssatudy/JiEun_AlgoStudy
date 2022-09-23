b30e1fbff0c2eb7eaa84f7aba5294659e85f2bfa
이지은 <jieun3181@gmail.com>
2022-09-23 17:53:56 +0900

    0923

---
+++ 0928/1759.망호만들기.py
@@ -0,0 +1,26 @@
+from sys import stdin
+def back(k):
+    ret = sorted(ans)
+    ret = ''.join(ret)
+    if k == m and  ret not in used:
+
+        used[ret] = 1
+        print(ret)
+        return
+    for i in range(n):
+        if i not in visited:
+            visited.append(i)
+            ans.append(lst[i])
+            back(k+1)
+            ans.pop()
+            visited.pop()
+
+
+m, n = map(int, stdin.readline().split())
+
+lst = sorted(list(stdin.readline().split()))
+
+ans = []
+visited = []
+used = {}
+back(0)
--- 0928/6603.로또.py
+++ 0928/6603.로또.py
@@ -1,22 +1,5 @@
-def back(k):
-    global used
-    if k == 6:
-        print(*ans)
+ans = ['a', 'd', 'c']
 
-        return
-    for i in range(l):
-        if lst[i] not in ans:
-            ans.append(lst[i])
-            back(k+1)
-            ans.pop()
-
-
-
-lst = list(map(int, input().split()))
-k = lst[0]
-lst = lst[1:len(lst)]
-l = len(lst)
-visited = []
-used = []
-ans = []
-back(0)
+ret = sorted(ans)
+ret  = ''.join(ret)
+print(ret)
