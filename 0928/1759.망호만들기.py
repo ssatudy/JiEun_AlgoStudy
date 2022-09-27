c89b882051e15cc720bd55e2423f90cf8ee39809
이지은 <jieun3181@gmail.com>
2022-09-27 17:52:08 +0900

    0927

--- 0928/1759.망호만들기.py
+++ 0928/1759.망호만들기.py
@@ -1,24 +1,22 @@
 from sys import stdin
-def back(k):
+def back(k, ans):
     ret = sorted(ans)
     ret = ''.join(ret)
-    if k == m and ret not in used:
+    if k == m and ret not in used and (ans.count('a') + ans.count('e') + ans.count('i') + ans.count('o') + ans.count('u')) >= 1:
         used[ret] = 1
         print(ret)
         return
     for i in range(n):
-        if i not in visited:
-            visited.append(i)
-            ans.append(lst[i])
-            back(k+1)
-            ans.pop()
-            visited.pop()
+        if visited[i] == 0:
+            visited[i] = 1
+            back(k+1, ans + [lst[i]])
+            visited[i] = 0
 
 
 m, n = map(int, stdin.readline().split())
 lst = sorted(list(stdin.readline().split()))
 
 ans = []
-visited = []
+visited = [0]*n
 used = {}
-back(0)
+back(0, [])
