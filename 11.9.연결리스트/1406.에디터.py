cfdb120ae02896fc491fa05d7996c3cda6d814a5
jieunlee0106 <jieun3181@gmail.com>
2022-11-07 17:34:25 +0900

    1107

--- 11.9.연결리스트/1406.에디터.py
+++ 11.9.연결리스트/1406.에디터.py
@@ -1,20 +1,24 @@
+
 word = list(input())
 cursor_idx = len(word)
+word_right = []
 
 for _ in range(int(input())):
     C = input()
     move = C[0]
-    if len(C) == 3:
-        add_word = C.pop()
     if move == 'L' and cursor_idx > 0:
+        word_right.append(word.pop())
         cursor_idx -= 1
     elif move == 'D' and cursor_idx < len(word):
+        word.append(word_right.pop())
         cursor_idx += 1
     elif move == 'B' and cursor_idx > 0:
-        word.pop(cursor_idx-1)
+        word.pop()
         cursor_idx -= 1
     elif move == 'P':
-        word.insert(cursor_idx, add_word)
+        word.append(C[2])
         cursor_idx += 1
 
+word_right = reversed(word_right)
+word.extend(word_right)
 print(''.join(word))
